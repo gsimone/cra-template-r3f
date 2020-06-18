b7f6bca538303123630307a4b7ebde0864db2966
Gianmarco Simone <gianmarcosimone89@gmail.com>
2020-06-18 17:56:14 +0200

    minor changes

--- src/App.js
+++ src/App.js
@@ -4,7 +4,14 @@ import { OrbitControls, StandardEffects, Box } from 'drei'
 
 function App() {
   return (
-    <Canvas shadowMap colorManagement camera={{ position: [0, 0, 17], far: 50 }}>
+    <Canvas
+      shadowMap
+      colorManagement
+      camera={{ position: [-4, 4, -4], far: 50 }}
+      style={{
+        background: "#121212"
+      }}
+    >
       <ambientLight />
       <spotLight
         intensity={2}
@@ -15,11 +22,14 @@ function App() {
         shadow-mapSize-height={2048}
         castShadow
       />
-      <Box>
-        <meshBasicMaterial attach="material" />
-      </Box>
+      <Box position={[-3, 0, 0]} />
+      <Box />
+      <Box position={[3, 0, 0]} />
+      {console.log('test')}
       <Suspense fallback={null}>
-        <StandardEffects bloom={{ luminanceThreshold: 0.99 }} />
+        <StandardEffects
+          smaa
+          bloom={{ luminanceThreshold: 0.99 }} />
       </Suspense>
       <OrbitControls />
     </Canvas>
